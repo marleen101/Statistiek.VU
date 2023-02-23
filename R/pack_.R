b98c982ffdb1e9b33f882e888d1422cb81e1e6d7
marleen101 <m.g.wolting@student.vu.nl>
2023-02-23 14:08:29 +0100

    functie aangemaakt om import bij NAMESPACE te gebruiken

---
+++ R/pack_.R
@@ -0,0 +1,10 @@
+#' Package die moeten in import
+#'
+#' @return text
+#' @export
+#' @import car corrplot DescTools dplyr expss ggplot2 gmodels janitor rio sjlabelled summarytools tidyverse zip Hmisc QuantPsyc
+#' @examples
+#' pack()
+pack <- function() {
+  print("meow")
+  }
