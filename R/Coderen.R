7b5af67304c79f99f04862bf94b59962ede24428
marleen101 <m.g.wolting@student.vu.nl>
2023-02-23 13:35:16 +0100

    hoofdletter gemaakt, zodat er geen error kwam

--- R/Coderen.R
+++ R/Coderen.R
@@ -4,7 +4,7 @@
 #' @export
 #'
 #' @examples
-#' coderen()
+#' Coderen()
 Coderen <- function() {
   print("eerst usethis::use_r(), daarna whoe je functie wil laten heten. met een pijltje er naar verwijzen en dan moet je bij function() bij zetten wat er moet gebeuren, zoals print(). dan als alles klaar is. roxygen skeleton doen en uitleg er bij schrijven. dan document() en een commit er bij zetten wat er gebeurt is. klaar!")
 }
